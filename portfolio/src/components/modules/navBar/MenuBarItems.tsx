b206fe0f8202a489e400190e982e3b9fa6ade6a7
shanker0811 <gourishanker697@gmail.com>
2023-08-29 22:23:03 +0530

    folderchanges

--- portfolio/src/components/modules/navBar/MenuBarItems.tsx
+++ portfolio/src/components/modules/navBar/MenuBarItems.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export default function MenuBarItems() {
     return (
         <div className='menuBarList'>
 
-            <Link to={'/'}> HOME</Link>
-            <Link to={'/secondComponet'}> EDUCATION</Link>
-            <Link to={'/secondComponet/childcomp2'}> PROJECTS </Link>
-            <Link to={'/secondComponet/childcomp2'}> WORKEXPERENCE </Link>
-            <Link to={'/secondComponet/childcomp2'}> View Resume</Link>
+            <NavLink to={'/'}> HOME</NavLink>
+            <NavLink to={'/education'}> EDUCATION</NavLink>
+            <NavLink to={'/projects'}> PROJECTS </NavLink>
+            <NavLink to={'/workExperience'}> WORKEXPERENCE </NavLink>
+            <NavLink to={'/Preview'}> View Resume</NavLink>
         </div>
     )
 }
---
+++ portfolio/src/components/modules/sections/homeSection/HomeSectionIndex.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HomeSectionIndex() {
+  return (
+    <div>HomeSectionIndex</div>
+  )
+}
